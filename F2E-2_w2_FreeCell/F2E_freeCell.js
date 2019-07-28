b73466e0627e92c3d3e769c2da644f97dcab06f5
aavail007 <aavail007@gmail.com>
2019-07-28 21:26:29 +0800

    大致完成

--- F2E-2_w2_FreeCell/F2E_freeCell.js
+++ F2E-2_w2_FreeCell/F2E_freeCell.js
@@ -1,74 +1,136 @@
 $(function () {
-    $("#playTimeBtn").click(function () {
-        MyCounter();
-        $(".playButton").addClass("hide");
-        $(".pauseButton").removeClass("hide");
-        $("#circle").addClass("time_ing").removeClass("time_play");
-    });
-    $("#pauseTimeBtn").click(function () {
-        stopCountDown();
-        $(".playButton").removeClass("hide");
-        $(".pauseButton").addClass("hide");
-        $("#circle").addClass("time_play").removeClass("time_ing");
+    $('#stopModal').modal('show');
+    //newGame();
+
+    $("#newGameBtn, #modalStart, #modalRestart").click(function () {
+        newGame();
+        $('#stopModal').modal('hide');
+        clearInterval(mm);
+        mm = setInterval("Check_Time()", 1000);
     });
 
-    $("#addTodoBtn").click(function () {
-        getTodoValue();
+    $("#keepPlaying").click(function () {
+        $('#stopModal').modal('hide');
+        mm = setInterval("Check_Time()", 1000);
     });
 
-    $("#restartBtn").click(function () {
-        stopCountDown();
-        time = 25 * 60 * 1000;
-        $("#countdownID").text("25:00");
-        $(".playButton").removeClass("hide");
-        $(".pauseButton").addClass("hide");
-        $("#circle").addClass("time_play").removeClass("time_ing");
+    $("#pause").click(function () {
+        clearInterval(mm);
+        $("#modalRestart, #keepPlaying").removeClass("hide");
+        $("#modalStart").addClass("hide");
+
     });
+
 
 });
 // =========================
-var time = 25 * 60 * 1000; //倒數25分鐘
-var countdownNum = '25:00';
-var setTimeoutFun;
-var todoListArray = [];
+var SetMinute = 0;
+var mm;
+var movesCount = 0;
 
-function MyCounter() {
-    if (time <= 0) {
-        //倒數完成
-        countdownNum = '05:00';
-    } else if (time > (60 * 1000)) {
-        if ((time % (60 * 1000)) / 1000 < 10) {
-            countdownNum = (parseInt((time / 1000) / 60) + ":0" + (time % (60 * 1000)) / 1000);
-        } else {
-            countdownNum = (parseInt((time / 1000) / 60) + ":" + (time % (60 * 1000)) / 1000);
-        }
-        setTimeoutFun = setTimeout(MyCounter, 1000);
-    } else {
-        if ((time % (60 * 1000)) / 1000 < 10) {
-            countdownNum = ("00:0" + (time % (60 * 1000)) / 1000);
-        } else {
-            countdownNum = ("00:" + (time % (60 * 1000)) / 1000);
-        }
-        setTimeoutFun = setTimeout(MyCounter, 1000);
-    }
-    time -= 1000;
-    $("#countdownID").text(countdownNum);
-
-
-};
-
-function stopCountDown() {
-    clearTimeout(setTimeoutFun);
+function getRandom(minNum, maxNum) { //取得 minNum(最小值) ~ maxNum(最大值) 之間的亂數
+    return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 }
 
-function getTodoValue() {
-    var todoValue = $("#todoInput").val();
-    todoListArray.push(todoValue);
-    var todoHtml = '<div class="todo_list_bottom mt-3">\
-                        <div class="circle-s"></div>\
-                        <div class="todo-word-s">' + todoValue + '</div>\
-                        <div class="inline play_icon_sm"><i class="far fa-play-circle"></i></div>\
-                    </div>'
-    $("#moreBtn").before(todoHtml);
+function getRandomArray(minNum, maxNum, n) { //隨機產生不重覆的n個數字
+    var rdmArray = [n]; //儲存產生的陣列
 
+    for (var i = 0; i < n; i++) {
+        var rdm = 0; //暫存的亂數
+
+        do {
+            var exist = false; //此亂數是否已存在
+            rdm = getRandom(minNum, maxNum); //取得亂數
+
+            //檢查亂數是否存在於陣列中，若存在則繼續回圈
+            if (rdmArray.indexOf(rdm) != -1) {
+                exist = true;
+            }
+
+        } while (exist); //產生沒出現過的亂數時離開迴圈
+
+        rdmArray[i] = rdm;
+    }
+    return rdmArray;
+}
+
+function dealCard() {
+    var cardArray = getRandomArray(1, 52, 52);
+    var rowNum = 1;
+    var zIndex = 0;
+    $(".card_border .poke_card_div").remove();
+    $('.card_position').empty();
+    for (var i = 0; i < cardArray.length; i++) {
+        zIndex = zIndex + 1;
+        var cardHtml = '<div class="poke_card_div card_row_' + zIndex + '" id="pokeID' + (i + 1) + '" draggable="true" ondragstart="Drag(event)"><img class="poke_card" src="card_img/Card-' + cardArray[i] + '.png"></div>'
+        $("#cardRow" + rowNum).append(cardHtml);
+        if (rowNum < 5) {
+            if ((i + 1) % 7 === 0) {
+                rowNum = rowNum + 1;
+                zIndex = 0;
+            }
+        } else if (rowNum == 5) {
+            if ((i + 1) % 7 === 6) {
+                rowNum = rowNum + 1;
+                zIndex = 0;
+            }
+        } else if (rowNum == 6) {
+            if ((i + 1) % 7 === 5) {
+                rowNum = rowNum + 1;
+                zIndex = 0;
+            }
+        } else {
+            if ((i + 1) % 7 === 4) {
+                rowNum = rowNum + 1;
+                zIndex = 0;
+            }
+        }
+
+    }
+    console.log(cardArray);
+}
+
+function Check_Time() {
+    SetMinute += 1;
+    var Check_i = document.getElementById("time");
+
+    var Cal_Hour = Math.floor(SetMinute / 3600);
+    var Cal_Minute = Math.floor(Math.floor(SetMinute % 3600) / 60);
+    var Cal_Second = SetMinute % 60;
+    if (Cal_Hour < 10) {
+        Cal_Hour = '0' + Cal_Hour;
+    }
+    if (Cal_Minute < 10) {
+        Cal_Minute = '0' + Cal_Minute;
+    }
+
+    if (Cal_Second < 10) {
+        Cal_Second = '0' + Cal_Second;
+    }
+
+    Check_i.innerHTML = Cal_Hour + ":" + Cal_Minute + ":" + Cal_Second;
+
+}
+
+function newGame() {
+    dealCard();
+    $("#time").text('00:00:00');
+    SetMinute = 0;
+}
+
+
+function AllowDrop(event) {
+    event.preventDefault();
+}
+
+function Drag(event) {
+    event.dataTransfer.setData("text", event.currentTarget.id);
+}
+
+function Drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.currentTarget.appendChild(document.getElementById(data));
+    movesCount = movesCount + 1;
+    $("#movesCount").text(movesCount);
 }
