0968270464704f92e39084caf5db12c83e3282b8
aavail007 <aavail007@gmail.com>
2019-08-30 00:37:46 +0800

    w7

---
+++ F2E-2_w7_ChatRoom/F2E-2_ChatRoom.js
@@ -0,0 +1,11 @@
+$(function () {
+
+
+});
+// =========================
+_.templateSettings = { //  underscore.js .Template method.
+    evaluate: /\{\{(.+?)\}\}/g,
+    interpolate: /\{\{=(.+?)\}\}/g,
+    escape: /\{\{-(.+?)\}\}/g
+};
+
